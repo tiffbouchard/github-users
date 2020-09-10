739e01db6e3aba7051af1e2e3740cd87691fa496
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-09-10 13:16:45 -0400

    finish styling

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const request = require('request');
+const request = require("request");
 const token = process.env.GITHUB_TOKEN;
-const rootURL = "https://api.github.com/"
-
+const rootURL = "https://api.github.com/";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // console.log(req.query);
   const username = req.query.username; //grabs url from query string in URL
   const options = {
     url: `${rootURL}users/${username}?access_token=${token}`,
     headers: {
-      'User-Agent': 'tiffbouchard'
-    }
-  }
-  request(
-    options,
-    function(err, response, body) {
-      console.log(body);
-      console.log(typeof body);
-      const userData = JSON.parse(body);
-      res.render('index', {userData: userData});
-    }
-  );
+      "User-Agent": "tiffbouchard",
+    },
+  };
+  request(options, function (err, response, body) {
+    const userData = JSON.parse(body);
+    options.url = userData.repos_url;
+    request(options, function (err, response, body) {
+      userData.repos = JSON.parse(body);
+      res.render("index", { userData: userData });
+    });
+  });
   // res.render('index', { title: 'Express' });
 });
 
